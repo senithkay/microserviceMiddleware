0eeb6ded6373670a34f54fa6af1435edc823a146
Senith Uthsara <senithkarunarathneu@gmail.com>
2023-11-05 21:13:51 +0530

    create middleware

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const app = express();
+const port = 3000;
+const session = require("express-session");
+
+app.use(
+  session({
+    secret: "your secret key",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
+app.use(function (req, res, next) {
+  if (req.session.isAuth) {
+    console.log("Session found");
+  } else {
+    console.log("Session not found");
+    //do auth
+    //and then
+    //if auth
+    req.session.isAuth = true;
+  }
+  next();
+});
+
+app.use(
+  "/getAllServices",
+  createProxyMiddleware({
+    target: "http://localhost:8080/api/v1/user/getAllServices",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/getAllServices": "/",
+    },
+  })
+);
+
+app.use(
+  "/activateService",
+  createProxyMiddleware({
+    target: "http://localhost:8080/api/v1/user/activateService",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/activateService": "/",
+    },
+  })
+);
+
+app.use(
+  "/deactivateService",
+  createProxyMiddleware({
+    target: "http://localhost:8080/api/v1/user/deactivateService",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/deactivateService": "/",
+    },
+  })
+);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
